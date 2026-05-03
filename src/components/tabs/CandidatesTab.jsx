e5dbf059d7cb44ae526013e560e6475e1053b0b0
zerokhdt <duonghieungockhdt@gmail.com>
2026-05-03 12:46:45 +0700

    update

--- src/components/tabs/CandidatesTab.jsx
+++ src/components/tabs/CandidatesTab.jsx
@@ -3,7 +3,7 @@ import { Download, Printer, Filter, Search, RefreshCw, BarChart3, Users, Trendin
 import CandidateDetailModal from '../modals/CandidateDetailModal';
 import { formatName, formatBranch, formatPosition } from '../../utils/formatters';
 import { downloadCSV } from '../../utils/exportCsv';
-import { collection, getDocs, writeBatch, doc, updateDoc } from "firebase/firestore";
+import { collection, getDocs, writeBatch, doc } from "firebase/firestore";
 import { db } from "../../utils/firebase";
 
 const CandidatesTab = ({ branches = [], isAdmin: _isAdmin = false, branchId: _branchId = '', onViewDetail, onMock }) => {
@@ -179,7 +179,6 @@ const CandidatesTab = ({ branches = [], isAdmin: _isAdmin = false, branchId: _br
                 : 'slate',
     original: candidate
   }));
-
   
 
   const onBulkAction = async (ids, action, extra) => {
